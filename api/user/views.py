1482027810ea8a09f4ac210359c6a979488772b2
Vignesh <contact@vigneshshetty.in>
2021-03-08 20:34:12 +0530

    token-generation

--- api/user/views.py
+++ api/user/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+import string, secrets
 
 # Create your views here.
+
+def generate_session_token(length=10):
+    alphabet = string.ascii_letters + string.digits
+    return ''.join(secrets.choice(alphabet) for i in range(length))
