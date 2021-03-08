1c3a407214379e47148b9190840ecae9e13a74e6
Vignesh <contact@vigneshshetty.in>
2021-03-08 20:22:10 +0530

    users-serializers

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'api',
     'api.category',
     'api.product'
+    # 'api.user'
 ]
 
 MIDDLEWARE = [
@@ -131,6 +132,9 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# AUTH_USER_MODEL="user.CustomUser"
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
