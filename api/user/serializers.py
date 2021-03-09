91b66b12cd0309979eaae3e3f9b93e47686398c9
Vignesh <contact@vigneshshetty.in>
2021-03-09 11:45:23 +0530

    postman-testing-api

--- api/user/serializers.py
+++ api/user/serializers.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import authentication_classes, permission_classes
 
 from .models import CustomUser
 
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     def create(self, validated_data):
@@ -26,5 +27,6 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = CustomUser
-        extra_kwargs = {'password':{'write_only':True}}
-        fields = ('name', 'emails','password', 'phone', 'gender', 'is_active', 'is_staff', 'is_superuser')
+        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('name', 'email', 'password', 'phone', 'gender',
+                  'is_active', 'is_staff', 'is_superuser')
