3720fc7959d7c79078511c4bd9fb36bc3c86dc2e
Vignesh <contact@vigneshshetty.in>
2021-03-09 12:15:59 +0530

    postman-testing

--- api/user/views.py
+++ api/user/views.py
@@ -2,57 +2,64 @@ from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from .serializers import UserSerializer
 from .models import CustomUser
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.contrib.auth import get_user_model
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import login, logout
-import re, string, secrets
-
+import random
+import re
 # Create your views here.
 
+
 def generate_session_token(length=10):
-    alphabet = string.ascii_letters + string.digits
-    return ''.join(secrets.choice(alphabet) for i in range(length))
+    return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)] + [str(i) for i in range(10)]) for _ in range(length))
+
 
 @csrf_exempt
 def signin(request):
-    if not request.method=='POST':
-        return JsonResponse({'error':'Send a post request with valid parameters only'})
+    if not request.method == 'POST':
+        return JsonResponse({'error': 'Send a post request with valid paramenter only'})
+
+    # print(request.POST.get('email', None))  - if you will not get email, None will be printed
     username = request.POST['email']
     password = request.POST['password']
 
-    if not re.match("^[\w\.-]+@[\w\.-]+\.\w{2,4}$", username):
-        return JsonResponse({'error':'Enter a vaid email'})
+    print(username)
+    print(password)
+
+# validation part
+    if not re.match("^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", username):
+        return JsonResponse({'error': 'Enter a valid email'})
 
     if len(password) < 3:
-        return JsonResponse({'error':'Password needs to be at least 3 char'})
+        return JsonResponse({'error': 'Password needs to be at least of 3 char'})
 
     UserModel = get_user_model()
-
 
     try:
         user = UserModel.objects.get(email=username)
 
         if user.check_password(password):
-            usr_dict = UserModel.objects.filter(email=username).values().first()
+            usr_dict = UserModel.objects.filter(
+                email=username).values().first()
             usr_dict.pop('password')
 
-            if user.session_token !="0":
+            if user.session_token != "0":
                 user.session_token = "0"
                 user.save()
-                return JsonResponse({'error':'Previous session exists'})
+                return JsonResponse({'error': "Previous session exists!"})
 
             token = generate_session_token()
             user.session_token = token
             user.save()
             login(request, user)
             return JsonResponse({'token': token, 'user': usr_dict})
-
         else:
-            return JsonResponse({'error':'Invalid password'})
+            return JsonResponse({'error': 'Invalid password'})
 
     except UserModel.DoesNotExist:
-        return JsonResponse({'error':'Invalid Email'})
+        return JsonResponse({'error': 'Invalid Email'})
+
 
 def signout(request, id):
     logout(request)
@@ -61,12 +68,14 @@ def signout(request, id):
 
     try:
         user = UserModel.objects.get(pk=id)
-        user.session_token="0"
+        user.session_token = "0"
         user.save()
+
     except UserModel.DoesNotExist:
         return JsonResponse({'error': 'Invalid user ID'})
 
     return JsonResponse({'success': 'Logout success'})
+
 
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes_by_action = {'create': [AllowAny]}
@@ -77,5 +86,6 @@ class UserViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         try:
             return [permission() for permission in self.permission_classes_by_action[self.action]]
+
         except KeyError:
             return [permission() for permission in self.permission_classes]
