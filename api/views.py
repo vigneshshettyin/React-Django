752bf758ddf60ee96f14c5d6308160eb58d586d3
Vignesh Shetty <vigneshshetty.in@gmail.com>
2021-04-06 10:02:14 +0530

    Config Env

--- api/views.py
+++ api/views.py
@@ -4,4 +4,4 @@ from django.http import JsonResponse
 
 
 def home(request):
-    return JsonResponse({'info': 'Django React Course', 'name': "hitesh"})
+    return JsonResponse({'info': 'Django React Course', 'name': "Vignesh"})
